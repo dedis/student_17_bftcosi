42b243f7de5722e2fce32d446360e619e63fba93
racater <raphael.dunant@epfl.ch>
2017-09-19 13:59:59 +0200

    finished protocol

--- protocol/gen_tree.go
+++ protocol/gen_tree.go
@@ -2,6 +2,7 @@ package protocol //TODO: protocol_test ?
 
 import (
 	"gopkg.in/dedis/onet.v1"
+	"gopkg.in/dedis/onet.v1/network"
 )
 
 // GenTree will create a tree of n servers with a localRouter, and returns the
@@ -14,14 +15,42 @@ func GenTree(l *onet.LocalTest, n_nodes, n_shards int, register bool) ([]*onet.S
 	}
 
 	//generate top-level of the tree
-	servers := l.GenServers(n_shards+1)
-	list := l.GenRosterFromHost(servers...)
-	tree := list.GenerateNaryTree(n_shards)
+	servers := l.GenServers(n_nodes)
+	n_top_level_nodes := n_shards+1
+	roster := l.GenRosterFromHost(servers[:n_top_level_nodes]...)
+	tree := roster.GenerateNaryTree(n_shards)
 	l.Trees[tree.ID] = tree
 	if register {
 		//servers[0].overlay.RegisterRoster(list)
 		//servers[0].overlay.RegisterTree(tree)
 	}
 
-	return servers, list, tree
+	if n_top_level_nodes != n_nodes {
+
+		nodes_per_shard := (n_nodes - 1) / n_shards
+
+		//generate each shard
+		for i, s := range servers[1:n_top_level_nodes] {
+			start := i*(nodes_per_shard-1) + n_top_level_nodes
+			end := start + (nodes_per_shard-1)
+			shards_servers := append(servers[start:end], s)
+
+			shard_roster := l.GenRosterFromHost(shards_servers...)
+			shard_roster.GenerateNaryTreeWithRoot(nodes_per_shard, s.ServerIdentity)
+			if register {
+				//servers[0].overlay.RegisterRoster(shard_list)
+				//servers[0].overlay.RegisterTree(shard_tree)
+			}
+		}
+
+		var server_identities []*network.ServerIdentity
+		for _, server := range servers {
+			server_identities = append(server_identities, server.ServerIdentity)
+		}
+
+		roster = onet.NewRoster(server_identities)
+		tree.Roster = roster
+	}
+
+	return servers, roster, tree
 }
